b5f0a3c2b71bbf1fa1b3a3012c66765277c37a42
easel7231 <a22yanzhayan@inspedralbes.cat>
2023-03-29 13:22:04 +0200

    player movement

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
-        //.add_system(click_to_move)
+        .add_system(move_player)
         .run();
 }
 
@@ -67,8 +67,37 @@ fn setup(
     });
 }
 
+pub const PLAYER_SPEED: f32 = 5.0;
+fn  move_player (
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+){
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-0.1, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(0.1, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(0.0, 0.0, 0.1);
+        }
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(0.0, 0.0, -0.1);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
 /* TODO Move the player towards the cursor position when the right mouse button is pressed
-fn click_to_move(
+fn click_tmovePLayero_move(
     mut events: EventReader<PickingEvent>,
     mut player: Query<(&mut Transform, &mut Player)>,
     primary_window: Query<&Window, With<PrimaryWindow>>,

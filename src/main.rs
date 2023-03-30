653f43189864c7c180d4fec0c38ce4aa2d574cf3
seitenca <alexdltg@protonmail.com>
2023-03-30 23:36:55 +0200

    feat: add camera rotation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::{input::mouse::MouseMotion, prelude::*, window::CursorGrabMode, window::PrimaryWindow};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use std::f32::consts::PI;
 
@@ -8,6 +8,7 @@ fn main() {
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
         .add_system(move_player)
+        .add_system(rotate_camera)
         .run();
 }
 
@@ -16,40 +17,25 @@ pub struct Player {
     speed: f32,
 }
 
-/// set up a simple 3D scene
+#[derive(Default, Component)]
+pub struct PlayerController {
+    yaw: Quat,
+    pitch: Quat,
+    cursor_locked: bool,
+}
+
 fn setup(
     mut commands: Commands,
     ass: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // note that we have to include the `Scene0` label
-    let my_gltf = ass.load("mereo.gltf#Scene0");
-    let slime = ass.load("slime.gltf#Scene0");
-
-    // Spawn the primary window
     commands.spawn(PrimaryWindow::default());
-
-
-    let player_spawn =  
-        Transform::from_xyz(0.0, 0.0, 0.0)
-        .with_rotation(Quat::from_rotation_y(PI/2.));
-
-    // Spawn the scene
-    commands.spawn((
-        SceneBundle {
-            scene: my_gltf,
-            transform: player_spawn,..default()
-        },
-        Player { speed: 20.0 },
-    ));
-
-    commands.spawn((SceneBundle {
-        scene: slime,
+    commands.spawn(SceneBundle {
+        scene: ass.load("slime.gltf#Scene0"),
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
-    },));
-    // light
+    });
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
@@ -59,57 +45,115 @@ fn setup(
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
-    // plane
+    commands.spawn(PlayerController::default());
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(50.0).into()),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
 
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0., 2., 8.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    commands
+        .spawn(SceneBundle {
+            scene: ass.load("mereo.gltf#Scene0"),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            computed_visibility: ComputedVisibility::default(),
+            global_transform: GlobalTransform::default(),
+            visibility: Visibility::default(),
+        })
+        .with_children(|parent| {
+            parent.spawn(Camera3dBundle {
+                projection: bevy::render::camera::Projection::Perspective(PerspectiveProjection {
+                    fov: PI / 2.,
+                    far: 2048.0,
+                    ..default()
+                }),
+                transform: Transform::from_xyz(0.0, 2.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
+                ..default()
+            });
+        })
+        .insert(Player { speed: 3.0 });
 }
 
-pub const PLAYER_SPEED: f32 = 5.0;
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut Transform, With<Player>>,
-    mut camera_query: Query<&mut Transform, (With<Camera3d>,Without<Player>)>,
+    mut player_query: Query<(&mut Transform, &Player)>,
     time: Res<Time>,
 ) {
-    if let Ok(mut player_transform) = player_query.get_single_mut() {
+    for (mut transform, player) in player_query.iter_mut() {
         let mut direction = Vec3::ZERO;
-        
+        let tr = transform.right();
+        let tf = transform.forward();
+
         if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-            direction += Vec3::new(-0.1, 0.0, 0.0);
+            direction -= Vec3::new(tr.x, 0.0, tr.z);
         }
         if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-            direction += Vec3::new(0.1, 0.0, 0.0);
+            direction += Vec3::new(tr.x, 0.0, tr.z);
         }
         if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-            direction += Vec3::new(0.0, 0.0, -0.1);
+            direction += Vec3::new(tf.x, 0.0, tf.z);
         }
         if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-            direction += Vec3::new(0.0, 0.0, 0.1);
+            direction -= Vec3::new(tf.x, 0.0, tf.z);
         }
-        if keyboard_input.pressed(KeyCode::Space)  {
+        if keyboard_input.pressed(KeyCode::Space) {
             direction += Vec3::new(0.0, 0.1, 0.0);
-        } 
-        if keyboard_input.pressed(KeyCode::V)  {
-            direction += Vec3::new(0.0, -0.1, 0.0);
-        } 
-        
+        }
+        if keyboard_input.pressed(KeyCode::LShift) {
+            direction -= Vec3::new(0.0, 0.1, 0.0);
+        }
+
         if direction.length() > 0.0 {
             direction = direction.normalize();
         }
 
-        player_transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+        transform.translation += direction * player.speed * time.delta_seconds();
+    }
+}
 
-        if let Ok(mut camera_transform) = camera_query.get_single_mut() {
-            camera_transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+fn rotate_camera(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut controller_query: Query<&mut PlayerController>,
+    mut mouse_events: EventReader<MouseMotion>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+    time: Res<Time>,
+) {
+    let mut delta: Vec2 = Vec2::ZERO;
+    let mut controller = controller_query.single_mut();
+
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        controller.cursor_locked = !controller.cursor_locked;
+    }
+
+    if controller.cursor_locked {
+        for mouse_move in mouse_events.iter() {
+            delta += mouse_move.delta;
+        }
+    }
+
+    if let Ok(mut window) = windows.get_single_mut() {
+        window.cursor.visible = !controller.cursor_locked;
+        window.cursor.grab_mode = if controller.cursor_locked {
+            CursorGrabMode::Confined
+        } else {
+            CursorGrabMode::None
+        };
+    }
+
+    for event in mouse_events.iter() {
+        delta += event.delta;
+    }
+
+    if delta.length() > 0.0 {
+        delta = delta * time.delta_seconds();
+        for mut transform in player_query.iter_mut() {
+            controller.yaw = Quat::from_rotation_y(-delta.x * 0.05);
+            controller.pitch = Quat::from_rotation_x(-delta.y * 0.05);
+
+            // Apply yaw and pitch to the camera's transform
+            transform.rotation = controller.yaw * transform.rotation;
+            transform.rotation = transform.rotation * controller.pitch;
         }
     }
 }
